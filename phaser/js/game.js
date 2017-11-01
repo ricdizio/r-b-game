8ad821993c70d8b13602febb550ae4da5a48f5bf
victorygc <victorygarciac@gmail.com>
2017-11-01 18:19:34 -0400

    Bloquea acciones mientras se juega, modificacion Layout

--- phaser/js/game.js
+++ phaser/js/game.js
@@ -5,8 +5,9 @@ var gameOptions = {
     gameHeight: 600,
     cardSheetWidth: 65,
     cardSheetHeight: 81,
-    cardScale: 1,
-    buttonScale: 0.4,
+    cardScaleOff: 0.5,
+    cardScaleOn: 1,
+    buttonScale: 0.1,
     flipZoom: 1.2,
     flipSpeed: 300
 }
@@ -19,6 +20,9 @@ window.onload = function() {
 var button;
 var flip = false;
 var cardNumber = 0;
+var playCard = false;
+var betNumber = -1;
+var cardsInGame = new Array();
 var playGame = function(game){}
 playGame.prototype = {
     preload: function() {
@@ -27,6 +31,7 @@ playGame.prototype = {
         game.load.image('ButtonB', 'assets/buttonB.png');
         game.load.spritesheet('cards0', 'assets/cards0.png', 334, 440);
         game.load.image('table', 'assets/table2.png');
+
         
         for(var i = 0; i < 51; i++){
             game.load.image("card" + i, "assets/card" + i + ".png", gameOptions.cardSheetWidth, gameOptions.cardSheetHeight);
@@ -34,27 +39,38 @@ playGame.prototype = {
     },
     create: function() {
         game.add.sprite(0,0,'table');
-        var buttonR = game.add.sprite(50,game.height-200,'ButtonR');
-        var buttonB = game.add.sprite(game.width-250,game.height-180,'ButtonB');
-        card =  game.add.sprite(game.width / 2, 0, "flip", 0);
-        card.anchor.set(0.5);
-        card.scale.set(gameOptions.cardScale);
+        card = this.makeCard();
+        var buttonR = game.add.sprite(game.width/2 - 100, game.height*3/4 + 60,'ButtonR');
+        var buttonB = game.add.sprite(game.width/2 + 100, game.height*3/4 + 60,'ButtonB');
+        var buttonE = game.add.sprite(0,0,'event');
+        buttonR.anchor.set(0.5);
+        buttonB.anchor.set(0.5);
+        buttonE.anchor.set(0.5);
+
         buttonR.scale.set(gameOptions.buttonScale);
         buttonB.scale.set(gameOptions.buttonScale);
-        buttonR = game.add.button(50,game.height-200, 'buttonR', this.onClickR, this, 0, 0, 0);
-        buttonB = game.add.button(game.width-250,game.height-180, 'buttonB', this.onClickB, this, 0, 0, 0);    
+        buttonE = game.add.button(0,0,'event', this.onClickEvent, this, 0, 0, 0);
+        buttonR = game.add.button(game.width/2 - 115, game.height*3/4 + 45, 'buttonR', this.onClickR, this, 0, 0, 0);
+        buttonB = game.add.button(game.width/2 + 85, game.height*3/4 + 45, 'buttonB', this.onClickB, this, 0, 0, 0);    
 
         card.isFlipping = false;
     },
-    onClickB: function(){
-        flip = !flip;
+    onClickR: function(){
+        console.log("RED BUTTON");
     },
     onClickB: function(){
-        flip = !flip;
+        if(!playCard){
+            flip = !flip;
+            playCard = true;
+        }
+        console.log("BLACK BUTTON");
+    },
+    onClickEvent: function(){
+        console.log("EVENT");
     },
     update: function() {
         this.card = card;
-        if(flip){
+        if(flip && playCard){
             this.flipTween = game.add.tween(this.card.scale).to({
                 x: 0,
                 y: gameOptions.flipZoom
@@ -104,17 +120,27 @@ playGame.prototype = {
             }, 500, Phaser.Easing.Linear.None, true);
         }
         game.time.events.add(Phaser.Timer.SECOND*2, function(){
-            card =  game.add.sprite(game.width / 2, 0, "flip", 0);
-            card.anchor.set(0.5);
+            playCard = false;
+            card.destroy();
+            card = this.makeCard();
+            //card =  game.add.sprite(game.width / 2, 0, "flip", 0);
+            //card.anchor.set(0.5);
             //game.state.start("PlayGame");    
         }, this)  
     },
-    makeCard: function(cardIndex) {
-        var card = game.add.sprite(gameOptions.cardSheetWidth * gameOptions.cardScale / -2, game.height / 2, "cards0");
+    makeCard: function() {
+        betNumber++;
+        cardsInGame.push(game.add.sprite(game.width / 2, game.height*3/4, "flip", 0));
+        //card =  game.add.sprite(game.width / 2, game.height*3/4, "flip", 0);
+        cardsInGame[betNumber].anchor.set(0.5);
+        cardsInGame[betNumber].scale.set(gameOptions.cardScaleOff);
+        return cardsInGame[betNumber];
+        /*
+        var card = game.add.sprite(gameOptions.cardSheetWidth * gameOptions.cardScaleOff / -2, game.height / 2, "cards0");
         card.anchor.set(0.5);
-        card.scale.set(gameOptions.cardScale);
+        card.scale.set(gameOptions.cardScaleOff);
         card.loadTexture("cards" + getCardTexture(deck[cardIndex]));
         card.frame = getCardFrame(deck[cardIndex]);
-        return card;
+        return card;*/
     }
 }
