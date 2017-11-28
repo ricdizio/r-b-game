5c10672fb668939ae8f711135ea8d32d070e235e
Noya <gabriel_noya@hotmail.com>
2017-11-27 23:12:00 -0400

    fix

--- Sails Server/assets/js/game.js
+++ Sails Server/assets/js/game.js
@@ -393,7 +393,7 @@ var playGame = {
       }
   },
   nickName: function(names){
-    for(var i = 0; i<names.length();i++){
+    for(var i = 0; i < names.length; i++){
       if(names[i])
         this.playerID[i].text = names[i];
     }
