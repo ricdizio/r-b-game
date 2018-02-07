a99a24fd8753345532bc032b468a3940f016cfca
victorygc <victorygarciac@gmail.com>
2018-02-07 12:49:57 -0400

    show timer fix

--- Sails Server/assets/js/playStage.js
+++ Sails Server/assets/js/playStage.js
@@ -190,31 +190,6 @@ var playGame = {
     this.gameParameters(type, capacity, rounds, time, nicksP, posP, gender1, money)
   },
   preload: function() {
-    // this.gameParameters()
-    // game.config.setForceTimeOut = true
-    // game.stage.disableVisibilityChange = true
-
-    // game.load.image('table', '../assets/mesa_ico.png')
-    // game.load.image('mplayer', '../assets/hombre_avatar_ico.png')
-    // game.load.image('wplayer', '../assets/mujer_avatar_ico.png')
-    // game.load.image('fplayer', '../assets/jugador_principal_ico.png')
-    // game.load.image('starWin', '../assets/star_ganador_ico.png')
-    // game.load.image('starLose', '../assets/star_perdedor_ico.png')
-    // game.load.image('mSmall', '../assets/hombre_small_ico.png')
-    // game.load.image('wSmall', '../assets/mujer_small_ico.png')
-    // game.load.image('cardBack', '../assets/carta_back_ico.png')
-    // game.load.image('coin', '../assets/disponible_ico.png')
-    // game.load.image('dupTime', '../assets/duplicar_tiempo_ico.png')
-    // game.load.spritesheet('buttonR', '../assets/btn_red_ico.png', 72, 42, 1)
-    // game.load.spritesheet('buttonL', '../assets/btn_leave_ico.png', 175, 47, 1)
-    // game.load.spritesheet('buttonB', '../assets/btn_black_ico.png', 72, 42, 1)
-    // game.load.spritesheet('buttonA', '../assets/btn_accept_ico.png', 77, 28, 1)
-    // game.load.spritesheet('buttonD', '../assets/btn_deny_ico.png', 77, 28, 1)
-    // game.load.spritesheet('buttonF', '../assets/btn_agregar_ico.png', 77, 28, 1)
-
-    // for(i = 0; i < 5; i++){
-    //   game.load.image("card" + i, "../assets/carta_" + (i+1) + ".png")
-    // }
   },
   // setPositions: function(nicks, myPos){
   //   this.nicks = nicks
@@ -348,32 +323,32 @@ var playGame = {
     this.addSprite(640, 809, 'dupTime')
     this.leaveBtn = game.add.button(928, 800, 'buttonL', this.shutdown, this, 0, 0, 0)   
 
-    // var countDown = this.playTime/ 1000
-    // this.timer.loop(1000,function(){
-    //   this.timerText.text = countDown--
-    //   if(countDown<1){
-    //     this.timer.stop()
-    //     this.timerText.text = ''
-    //   }
-    // }, this)
-    // this.timerText.text = countDown--
-    // this.timer.start()
+    var countDown = this.playTime/ 1000
+    this.timer.loop(1000,function(){
+      this.timerText.text = countDown--
+      if(countDown<1){
+        this.timer.stop()
+        this.timerText.text = ''
+      }
+    }, this)
+    //this.timerText.text = countDown--
+    this.timer.start()
 
-    // game.time.events.add(Phaser.Timer.SECOND*5, function(){
-    //   this.btnUpdate(this.btnEnum.PICK, true)
-    //   this.timerOn = false
-    //   this.timer.stop()
-    //   this.timerText.text = ''
-    //   this.timerCircle.clear()
-    // }, this)
+    game.time.events.add(Phaser.Timer.SECOND*5, function(){
+      this.btnUpdate(this.btnEnum.PICK, true)
+      this.timerOn = false
+      this.timer.stop()
+      this.timerText.text = ''
+      this.timerCircle.clear()
+    }, this)
   
     // var winArr = new Array(1,2)
     // this.updateWinners("",0,winArr,0)
     // var winArr1 = new Array(0,1)
     // this.updateWinners("",0,winArr1,4)
     game.world.bringToTop(this.timerCircle)
-    game.world.bringToTop(this.timerText)
     game.world.bringToTop(this.radialProgressBar)
+    game.world.bringToTop(this.timerText)
   },
   updateStatus: function(player, status){
     var posX, posY
@@ -466,9 +441,10 @@ var playGame = {
     this.btnUpdate(this.btnEnum.PICK, true)
     this.timerBar = game.add.tween(this.timerAngle).to( { max: 360 }, this.playTime, "Linear", true, 0, 0, false)
     this.timerOn = true
-    // game.world.bringToTop(this.timerCircle)
-    // game.world.bringToTop(this.radialProgressBar)
-    // game.world.bringToTop(this.timerText)
+    this.timer.start()
+    game.world.bringToTop(this.timerCircle)
+    game.world.bringToTop(this.radialProgressBar)
+    game.world.bringToTop(this.timerText)
     this.timerBar.onComplete.add(function(){
       this.timerOn = false
       this.timerCircle.clear()
