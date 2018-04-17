ba153e3195778d9a3211e528e25995ba63344f47
Gabriel <gabrielnoya95@gmail.com>
2018-04-17 17:52:06 -0400

    sumar y restar dinero a base de datos

--- Sails Server/api/services/Database.js
+++ Sails Server/api/services/Database.js
@@ -11,10 +11,40 @@ module.exports = {
       return true;
     });  
   },
+
   addMoney: function(id, money){
+    User.findOne({id: id}).exec(function(err, user) {
+      if (err) return next(err);
+      if (!user) return res.serverError('User doesn\'t exist.');
 
+      let userObj = { tokens: (user.tokens + money)};
+
+      User.update({id: id}, userObj).exec(function afterwards(err, updated){
+        if (err) {
+          console.log(err);
+          return false;
+        }
+        console.log(updated);
+        return true;
+      });  
+    });
   },
-  substractMoney: function(id, money){
 
-  }
+  substractMoney: function(id, money){
+    User.findOne({id: id}).exec(function(err, user) {
+      if (err) return next(err);
+      if (!user) return res.serverError('User doesn\'t exist.');
+
+      let userObj = { tokens: (user.tokens - money)};
+
+      User.update({id: id}, userObj).exec(function afterwards(err, updated){
+        if (err) {
+          console.log(err);
+          return false;
+        }
+        console.log(updated);
+        return true;
+      });  
+    });
+  },
 }
